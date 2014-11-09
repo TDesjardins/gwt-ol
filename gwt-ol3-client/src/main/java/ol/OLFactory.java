1c0d89104407b42edbf0c564904ec3a3e4a7b5c0
Tino Desjardins <tino.desjardins@arcor.de>
2014-11-09 18:01:17 +0100

    Added creation support for ImageWMS, params and options.

--- gwt-ol3-client/src/main/java/ol/OLFactory.java
+++ gwt-ol3-client/src/main/java/ol/OLFactory.java
@@ -8,14 +8,19 @@ import ol.control.ZoomSlider;
 import ol.control.ZoomToExtent;
 import ol.interaction.KeyboardPan;
 import ol.interaction.KeyboardZoom;
+import ol.layer.Image;
 import ol.layer.LayerOptions;
 import ol.layer.Tile;
+import ol.proj.Projection;
+import ol.proj.ProjectionOptions;
+import ol.source.ImageWMS;
+import ol.source.ImageWMSOptions;
 import ol.source.MapQuest;
 import ol.source.MapQuestOptions;
 import ol.source.Stamen;
 import ol.source.StamenOptions;
-import ol.source.StaticImage;
-import ol.source.StaticImageOptions;
+import ol.source.ImageStatic;
+import ol.source.ImageStaticOptions;
 
 /**
  * Factory to create GWT-OL3-Object instances from JavaScript based on OL3-Interfaces.
@@ -39,6 +44,10 @@ public class OLFactory {
 
     /** Layers **/
 
+    public static native <T> Image createImageLayer(LayerOptions layerOptions) /*-{
+        return new $wnd.ol.layer.Image(layerOptions);
+    }-*/;
+    
     public static native <T> Tile createTileLayer(LayerOptions layerOptions) /*-{
 		return new $wnd.ol.layer.Tile(layerOptions);
     }-*/;
@@ -49,9 +58,13 @@ public class OLFactory {
 
     /** Sources **/
 
-    public static native <T> StaticImage createStaticImageSource(StaticImageOptions staticImageOptions) /*-{
-		return new $wnd.ol.source.StaticImage(staticImageOptions);
+    public static native <T> ImageStatic createImageStaticSource(ImageStaticOptions imageStaticOptions) /*-{
+		return new $wnd.ol.source.ImageStatic(imageStaticOptions);
 	}-*/;
+    
+    public static native <T> ImageWMS createImageWMSSource(ImageWMSOptions imageWMSOptions) /*-{
+        return new $wnd.ol.source.ImageWMS(imageWMSOptions);
+    }-*/;
     
     public static native <T> MapQuest createMapQuestSource(MapQuestOptions mapQuestOptions) /*-{
 		return new $wnd.ol.source.MapQuest(mapQuestOptions);
@@ -69,10 +82,20 @@ public class OLFactory {
         return {};
     }-*/;
 
+    /** Projection **/
+    
+    public static native <T> Projection createProjection(ProjectionOptions projectionOptions) /*-{
+        return new $wnd.ol.proj.Projection(projectionOptions);
+    }-*/;
+    
     /** View **/
 
     public static native <T> View createView() /*-{
 		return new $wnd.ol.View();
+    }-*/;
+    
+    public static native <T> View createView(ViewOptions viewOptions) /*-{
+        return new $wnd.ol.View(viewOptions);
     }-*/;
 
     /** Controls **/
@@ -119,6 +142,15 @@ public class OLFactory {
      * @return common options object
      */
     public static native <T> T createOptions() /*-{
+        return {};
+    }-*/;
+    
+    /**
+     * Creates a common object for params.
+     *
+     * @return common params object
+     */
+    public static native <T> T createParams() /*-{
         return {};
     }-*/;
 
