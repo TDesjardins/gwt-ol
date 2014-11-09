4577a2c45345f303670a4d7e7894b7830abe6a16
Tino Desjardins <tino.desjardins@arcor.de>
2014-11-09 18:17:42 +0100

    Added WMS-example

--- gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/GwtOL3Playground.java
+++ gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/GwtOL3Playground.java
@@ -6,8 +6,15 @@ import ol.Map;
 import ol.MapOptions;
 import ol.OLFactory;
 import ol.View;
+import ol.ViewOptions;
+import ol.layer.Image;
 import ol.layer.LayerOptions;
 import ol.layer.Tile;
+import ol.proj.Projection;
+import ol.proj.ProjectionOptions;
+import ol.source.ImageWMS;
+import ol.source.ImageWMSOptions;
+import ol.source.ImageWMSParams;
 import ol.source.MapQuest;
 import ol.source.MapQuestOptions;
 import ol.source.Stamen;
@@ -23,6 +30,92 @@ public class GwtOL3Playground implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
+
+        // choose your example
+        OL3ExampleType exampleType = OL3ExampleType.TileExample;
+        
+        switch (exampleType) {
+            case WmsExample: 
+                createMapWithWmsConfiguration();
+                break;
+            case TileExample:
+                createMapWithTileConfiguration();
+                break;
+            default:
+                createMapWithTileConfiguration();
+        }
+
+    }
+    
+    /**
+     * Creates a map with a WMS-layer.
+     */
+    private static void createMapWithWmsConfiguration() {
+
+        ImageWMSParams imageWMSParams = OLFactory.createParams();
+        imageWMSParams.setLayers("ch.swisstopo.geologie-geotechnik-gk500-gesteinsklassierung,ch.bafu.schutzgebiete-paerke_nationaler_bedeutung");
+        
+        ImageWMSOptions imageWMSOptions = OLFactory.createOptions();
+        imageWMSOptions.setUrl("http://wms.geo.admin.ch/");
+        imageWMSOptions.setParams(imageWMSParams);
+        imageWMSOptions.setRatio(1.5f);
+
+        ImageWMS imageWMSSource = OLFactory.createImageWMSSource(imageWMSOptions);
+
+        LayerOptions layerOptions = OLFactory.createLayerOptions();
+        layerOptions.setSource(imageWMSSource);
+        
+        Image wmsLayer = OLFactory.createImageLayer(layerOptions);
+        
+        // create a projection
+        
+        ProjectionOptions projectionOptions = OLFactory.createOptions();
+        projectionOptions.setCode("EPSG:21781");
+        projectionOptions.setUnits("m");
+        
+        Projection projection = OLFactory.createProjection(projectionOptions);
+        
+        // create a view
+        
+        ViewOptions viewOptions = OLFactory.createOptions();
+        viewOptions.setProjection(projection);
+        View view = OLFactory.createView(viewOptions);
+
+        double[] coords = new double[2];
+
+        coords[0] = 660000;
+        coords[1] = 190000;
+
+        view.setCenter(coords);
+        view.setZoom(9);
+
+        // create the map
+        MapOptions mapOptions = OLFactory.createMapOptions();
+        mapOptions.setTarget("map");
+        mapOptions.setView(view);
+
+        Map map = OLFactory.createMap(mapOptions);
+
+        map.addLayer(wmsLayer);
+
+        // add some controls
+        map.addControl(OLFactory.createFullScreen());
+        map.addControl(OLFactory.createScaleLine());
+        map.addControl(OLFactory.createZoomSlider());
+        map.addControl(OLFactory.createMousePosition());
+        map.addControl(OLFactory.createZoomToExtent());
+
+        // add some interactions
+        map.addInteraction(OLFactory.createKeyboardPan());
+        map.addInteraction(OLFactory.createKeyboardZoom());
+        map.addControl(OLFactory.createRotate());
+        
+    }
+    
+    /**
+     * Creates a map with Tile-layers.
+     */
+    private static void createMapWithTileConfiguration() {
 
         // create a MapQuest-layer
         LayerOptions mapQuestLayerOptions = OLFactory.createLayerOptions();
---
+++ gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/OL3ExampleType.java
@@ -0,0 +1,15 @@
+package de.desjardins.ol3.demo.client;
+
+/**
+ * Provided example types.
+ *
+ * @author Tino Desjardins
+ *
+ */
+public enum OL3ExampleType {
+    
+    WmsExample,
+    TileExample
+    
+}
+
