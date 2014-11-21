07184c06e2414ebe1d2cba14980df77f1cd94a26
Tino Desjardins <tino.desjardins@arcor.de>
2014-11-21 21:54:57 +0100

    Preparations for testing

---
+++ gwt-ol3-client/src/test/java/ol/BaseTestCase.java
@@ -0,0 +1,17 @@
+package ol;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * 
+ * @author Tino Desjardins
+ *
+ */
+public class BaseTestCase extends GWTTestCase {
+
+    @Override
+    public String getModuleName() {
+        return "OLGwt3Test";
+    }
+
+}
---
+++ gwt-ol3-client/src/test/java/ol/MapTest.java
@@ -0,0 +1,40 @@
+package ol;
+
+import ol.BaseTestCase;
+
+/**
+ * 
+ * @author Tino Desjardins
+ *
+ */
+public class MapTest extends BaseTestCase {
+
+    public void testMapCreation() {
+        
+        // TODO Add OL3-script.
+        
+        /* ProjectionOptions projectionOptions = OLFactory.createOptions();
+        projectionOptions.setCode("EPSG:21781");
+        projectionOptions.setUnits("m");
+        
+        Projection projection = OLFactory.createProjection(projectionOptions);
+        
+        ViewOptions viewOptions = OLFactory.createOptions();
+        viewOptions.setProjection(projection);
+        View view = OLFactory.createView(viewOptions);
+
+        double[] centerCoordinate = OLFactory.createCoordinate(660000, 190000);
+        
+        view.setCenter(centerCoordinate);
+        view.setZoom(9);
+        
+        MapOptions mapOptions = OLFactory.createMapOptions();
+        mapOptions.setTarget("map");
+        mapOptions.setView(view);
+        
+        Map map = Map.createInstance(mapOptions);
+        assertNotNull(map); */
+        
+    }
+
+}
