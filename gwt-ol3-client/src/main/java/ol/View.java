df35f2d04397e0ff2db0c1035cc86ac43f717b50
dejavue88 <dejavue88@googlemail.com>
2015-02-03 12:17:26 +0100

    Added method for setting projection Formatted

--- gwt-ol3-client/src/main/java/ol/View.java
+++ gwt-ol3-client/src/main/java/ol/View.java
@@ -1,24 +1,26 @@
 package ol;
 
+import com.google.gwt.core.client.js.JsProperty;
 import com.google.gwt.core.client.js.JsType;
 
 import ol.proj.Projection;
 
 /**
- * 
  * @author Tino Desjardins
- *
  */
-@JsType(prototype="View")
+@JsType(prototype = "View")
 public interface View extends Object {
 
     void rotate(float rotation, double[] coordinate);
-    
+
     void setCenter(double[] center);
-    
+
     void setZoom(double zoom);
-    
+
     Projection getProjection();
-    
+
+    @JsProperty
+    void setProjection(Projection projection);
+
 }
 
