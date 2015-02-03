3b567367b4958a997a0da3973c7451be39ccc5a4
dejavue88 <dejavue88@googlemail.com>
2015-02-03 12:02:21 +0100

    Added Fluent Interface Support

--- gwt-ol3-client/src/main/java/ol/proj/ProjectionOptions.java
+++ gwt-ol3-client/src/main/java/ol/proj/ProjectionOptions.java
@@ -4,21 +4,34 @@ import com.google.gwt.core.client.js.JsProperty;
 import com.google.gwt.core.client.js.JsType;
 
 /**
- * 
  * @author Tino Desjardins
- *
  */
 @JsType
 public interface ProjectionOptions {
-    
-	@JsProperty
-	void setCode(String code);
-    
-	@JsProperty
+
+    @JsProperty
+    void setCode(String code);
+
+    @JsProperty
+    ProjectionOptions code(String code);
+
+    @JsProperty
     void setUnits(String units);
-	
-	@JsProperty
+
+    @JsProperty
+    ProjectionOptions units(String units);
+
+    @JsProperty
     void setGlobal(boolean global);
-	
+
+    @JsProperty
+    ProjectionOptions global(boolean global);
+
+    @JsProperty
+    void setExtent(double[] extent);
+
+    @JsProperty
+    ProjectionOptions extent(double[] extent);
+
 }
 
