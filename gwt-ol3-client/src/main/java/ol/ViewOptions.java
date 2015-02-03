d5bff3a3f4912af2aad5ec8e587436e4f36aa322
dejavue88 <dejavue88@googlemail.com>
2015-02-03 12:15:04 +0100

    Added some options Added doc

--- gwt-ol3-client/src/main/java/ol/ViewOptions.java
+++ gwt-ol3-client/src/main/java/ol/ViewOptions.java
@@ -6,15 +6,20 @@ import com.google.gwt.core.client.js.JsType;
 import ol.proj.Projection;
 
 /**
+ * Options for creating a {@link ol.View}.
  * 
  * @author Tino Desjardins
- *
  */
 @JsType
 public interface ViewOptions {
-    
-	@JsProperty
-	void setProjection(Projection projection);
-	
-}
 
+    @JsProperty
+    void setCenter(double[] center);
+
+    @JsProperty
+    void setProjection(Projection projection);
+
+    @JsProperty
+    void setZoom(double zoom);
+
+}
