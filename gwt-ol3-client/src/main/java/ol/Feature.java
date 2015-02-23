bc54ee278ce24147ec623090d8b6c12bd829245f
TDesjardins <tino.desjardins@arcor.de>
2015-02-23 23:04:48 +0100

    Added methods

--- gwt-ol3-client/src/main/java/ol/Feature.java
+++ gwt-ol3-client/src/main/java/ol/Feature.java
@@ -2,6 +2,8 @@ package ol;
 
 import com.google.gwt.core.client.js.JsType;
 
+import ol.geom.Geometry;
+
 /**
  * 
  * @author Tino Desjardins
@@ -9,5 +11,20 @@ import com.google.gwt.core.client.js.JsType;
  */
 @JsType
 public interface Feature extends Object {
+    
+    Feature clone();
+    
+    Geometry getGeometry();
+    
+    String getGeometryName();
+    
+    String getId();
+    
+    void setGeometry(Geometry geometry);
+    
+    void setGeometryName(String geometryName);
+    
+    void setId(String id);
+    
 }
 
