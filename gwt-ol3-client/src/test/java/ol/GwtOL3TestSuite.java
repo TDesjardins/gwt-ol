1ac84ea3c3dc4c06872db1e4f5cc3d942b15c91c
TDesjardins <tino.desjardins@arcor.de>
2015-02-23 21:53:06 +0100

    Added tests

--- gwt-ol3-client/src/test/java/ol/GwtOL3TestSuite.java
+++ gwt-ol3-client/src/test/java/ol/GwtOL3TestSuite.java
@@ -4,13 +4,20 @@ import com.google.gwt.junit.tools.GWTTestSuite;
 
 import junit.framework.TestSuite;
 
-
+/**
+ * Testsuite for GwtOL3.
+ * 
+ * @author Tino Desjardins
+ *
+ */
 public class GwtOL3TestSuite extends GWTTestSuite {
 
     public static TestSuite suite() {
         
         TestSuite suite = new TestSuite("Tests for GwtOL3-Wrapper");
         
+        suite.addTestSuite(AttributionTest.class);
+        suite.addTestSuite(ol.control.AttributionTest.class);
         suite.addTestSuite(MapTest.class);
         suite.addTestSuite(FeatureOverlayTest.class);
         suite.addTestSuite(ViewTest.class);
