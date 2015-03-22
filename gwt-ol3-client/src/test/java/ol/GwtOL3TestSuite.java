1a583e303b8f0391500f6728d5c3e98535c95bd6
Tino Desjardins <tino.desjardins@arcor.de>
2015-03-22 11:16:02 +0100

    Added tests

--- gwt-ol3-client/src/test/java/ol/GwtOL3TestSuite.java
+++ gwt-ol3-client/src/test/java/ol/GwtOL3TestSuite.java
@@ -2,6 +2,16 @@ package ol;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
 
+import ol.control.FullScreenTest;
+import ol.control.MousePositionTest;
+import ol.control.RotateTest;
+import ol.control.ScaleLineTest;
+import ol.control.ZoomSliderTest;
+import ol.control.ZoomToExtentTest;
+import ol.interaction.DragAndDropTest;
+import ol.interaction.KeyboardPanTest;
+import ol.interaction.KeyboardZoomTest;
+import ol.interaction.ModifyTest;
 import junit.framework.TestSuite;
 
 /**
@@ -17,10 +27,24 @@ public class GwtOL3TestSuite extends GWTTestSuite {
         TestSuite suite = new TestSuite("Tests for GwtOL3-Wrapper");
         
         suite.addTestSuite(AttributionTest.class);
-        suite.addTestSuite(ol.control.AttributionTest.class);
         suite.addTestSuite(MapTest.class);
         suite.addTestSuite(FeatureOverlayTest.class);
         suite.addTestSuite(ViewTest.class);
+        
+        // controls
+        suite.addTestSuite(ol.control.AttributionTest.class);
+        suite.addTestSuite(FullScreenTest.class);
+        suite.addTestSuite(MousePositionTest.class);
+        suite.addTestSuite(RotateTest.class);
+        suite.addTestSuite(ScaleLineTest.class);
+        suite.addTestSuite(ZoomSliderTest.class);
+        suite.addTestSuite(ZoomToExtentTest.class);
+        
+        // interactions
+        suite.addTestSuite(DragAndDropTest.class);
+        suite.addTestSuite(KeyboardPanTest.class);
+        suite.addTestSuite(KeyboardZoomTest.class);
+        suite.addTestSuite(ModifyTest.class);
         
         return suite;
         
