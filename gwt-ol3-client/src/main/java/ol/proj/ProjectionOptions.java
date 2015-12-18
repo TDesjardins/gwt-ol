bad151b11d543ebe6f911c40bc24a30edc57abc4
Tino Desjardins <tino.desjardins@arcor.de>
2015-12-18 13:20:47 +0100

    organize imports

--- gwt-ol3-client/src/main/java/ol/proj/ProjectionOptions.java
+++ gwt-ol3-client/src/main/java/ol/proj/ProjectionOptions.java
@@ -3,7 +3,8 @@ package ol.proj;
 import com.google.gwt.core.client.js.JsProperty;
 import com.google.gwt.core.client.js.JsType;
 
-import ol.*;
+import ol.Extent;
+import ol.Options;
 
 /**
  * @author Tino Desjardins
