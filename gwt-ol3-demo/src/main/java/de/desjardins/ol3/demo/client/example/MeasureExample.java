3385638c84d7d2abf8857ff3752850823c38dfb2
Sebastian Baumhekel <sebastian.baumhekel@gmail.com>
2016-01-04 08:36:19 +0100

    example for measuring.

---
+++ gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/example/MeasureExample.java
@@ -0,0 +1,94 @@
+package de.desjardins.ol3.demo.client.example;
+
+import ol.Coordinate;
+import ol.Map;
+import ol.MapOptions;
+import ol.OLFactory;
+import ol.OLUtil;
+import ol.View;
+import ol.control.Attribution;
+import ol.event.*;
+import ol.gwt.Measure;
+
+import com.google.gwt.core.client.GWT;
+
+import de.desjardins.ol3.demo.client.utils.DemoUtils;
+import ol.layer.Tile;
+import ol.source.Osm;
+import ol.source.TileDebug;
+import ol.source.TileDebugOptions;
+import ol.source.XyzOptions;
+import ol.layer.LayerOptions;
+
+/**
+ * Example measuring on an OSM layer.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+public class MeasureExample implements Example {
+
+    /* (non-Javadoc)
+     * @see de.desjardins.ol3.demo.client.example.Example#show()
+     */
+    @Override
+    public void show() {
+        
+        // create a OSM-layer
+        XyzOptions osmSourceOptions = OLFactory.createOptions();
+        
+        Osm osmSource = OLFactory.createOsm(osmSourceOptions);
+        LayerOptions osmLayerOptions = OLFactory.createOptions();
+        osmLayerOptions.setSource(osmSource);
+        
+        Tile osmLayer = OLFactory.createTileLayer(osmLayerOptions);
+
+        // create a view
+        View view = OLFactory.createView();
+
+        Coordinate centerCoordinate = OLFactory.createCoordinate(-0.1275, 51.507222);
+        Coordinate transformedCenterCoordinate = OLUtil.transform(centerCoordinate, "EPSG:4326", "EPSG:3857"); 
+        
+        view.setCenter(transformedCenterCoordinate);
+        view.setZoom(10);
+
+        // create the map
+        MapOptions mapOptions = OLFactory.createOptions();
+        mapOptions.setTarget("map");
+        mapOptions.setView(view);
+
+        Map map = OLFactory.createMap(mapOptions);
+        
+        map.addLayer(osmLayer);
+
+        // add some controls
+        map.addControl(OLFactory.createScaleLine());
+        DemoUtils.addDefaultControls(map.getControls());
+        
+        Attribution attribution = OLFactory.createAttributionControl();
+        attribution.setCollapsed(true);
+        
+        map.addControl(attribution);
+        
+        // add some interactions
+        map.addInteraction(OLFactory.createKeyboardPan());
+        map.addInteraction(OLFactory.createKeyboardZoom());
+     
+		// add measurement functionality to the map
+		final Measure measure = new Measure(map);
+		// start measuring immediately
+		measure.startMeasureLength(new MeasureListener()
+		{
+
+			@Override
+			public void onMeasure(MeasureEvent evt)
+			{
+				// log the measured length
+				GWT.log("measure: " + evt.getMeasure() + " Lat/Lon: " + evt.getMeasure("EPSG:4326"));
+			}
+		}, true, true);
+        
+    }
+    
+}
+
