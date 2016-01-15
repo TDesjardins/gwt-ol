cb18551dc704ef6ffb98d55ab6cbcce390ffc767
AAauAAau <lochmann.tobias@gmail.com>
2016-01-15 09:49:51 +0100

    added image source for MapGuide

---
+++ gwt-ol3-client/src/main/java/ol/source/ImageMapGuide.java
@@ -0,0 +1,13 @@
+package ol.source;
+
+import com.google.gwt.core.client.js.JsType;
+
+/**
+ * Source for images from Mapguide servers.
+ * @author tlochmann
+ */
+@JsType(prototype = "ol.source.ImageMapGuide")
+public interface ImageMapGuide extends Image {
+   
+    
+}
