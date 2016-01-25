b7589e4a69059837f30d79332992da26dc2aaf05
Sebastian Baumhekel <sebastian.baumhekel@gmail.com>
2016-01-25 11:29:37 +0100

    provide the currently used projection in the MeasureEvent. this allows to change the map view projection while still getting correct measurements.

--- gwt-ol3-client/src/main/java/ol/event/MeasureEvent.java
+++ gwt-ol3-client/src/main/java/ol/event/MeasureEvent.java
@@ -1,6 +1,8 @@
 package ol.event;
 
+import ol.OLUtil;
 import ol.geom.Geometry;
+import ol.proj.Projection;
 
 /**
  * An event for measuring.
@@ -9,20 +11,21 @@ import ol.geom.Geometry;
  */
 public class MeasureEvent {
 
-    /**
-     * Map projection code.
-     */
-    private static final String MAP_PROJECTION = "EPSG:3857";
     private final Geometry geom;
+    private final Projection projGeom;
 
     /**
      * Constructs an instance.
+     * 
+     * @param projGeom
+     *            {@link Projection} of the geometry
      *
      * @param geom
      *            measure {@link Geometry}
      */
-    public MeasureEvent(Geometry geom) {
+    public MeasureEvent(Projection projGeom, Geometry geom) {
         super();
+        this.projGeom = projGeom;
         this.geom = geom;
     }
 
@@ -34,19 +37,19 @@ public class MeasureEvent {
      * @return measure on success, else {@link Double#NaN}
      */
     private static double getMeasure(ol.geom.Geometry geom) {
-        if (geom instanceof ol.geom.LineString) {
-            ol.geom.LineString ls = (ol.geom.LineString) geom;
+        if(geom instanceof ol.geom.LineString) {
+            ol.geom.LineString ls = (ol.geom.LineString)geom;
             return ls.getLength();
-        } else if (geom instanceof ol.geom.Polygon) {
-            ol.geom.Polygon poly = (ol.geom.Polygon) geom;
+        } else if(geom instanceof ol.geom.Polygon) {
+            ol.geom.Polygon poly = (ol.geom.Polygon)geom;
             return poly.getArea();
         }
         return Double.NaN;
     }
 
     /**
-     * Gets the measurement geometry: a {@link LineString} for length
-     * measurements and a {@link Polygon} for area measurements.
+     * Gets the measurement geometry: a {@link ol.geom.LineString} for length
+     * measurements and a {@link ol.geom.Polygon} for area measurements.
      * 
      * @return {@link Geometry}
      */
@@ -71,8 +74,29 @@ public class MeasureEvent {
      * @return measure on success, else {@link Double#NaN}
      */
     public double getMeasure(String proj) {
-        Geometry geom = this.geom.clone().transform(MAP_PROJECTION, proj);
+        Geometry geom = this.geom.clone().transform(this.projGeom.getCode(), proj);
         return getMeasure(geom);
+    }
+
+    /**
+     * Gets the current measure in the given projection (and its unit).
+     *
+     * @param proj
+     *            projection
+     * @return measure on success, else {@link Double#NaN}
+     */
+    public double getMeasure(Projection proj) {
+        Geometry geom = OLUtil.transform(this.geom.clone(), this.projGeom, proj);
+        return getMeasure(geom);
+    }
+
+    /**
+     * Gets the {@link Projection} of the geometry.
+     * 
+     * @return {@link Projection}
+     */
+    public Projection getProjection() {
+        return this.projGeom;
     }
 
 }
