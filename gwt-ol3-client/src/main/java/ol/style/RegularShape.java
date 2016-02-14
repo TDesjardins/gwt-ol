43e6b2486d14acc29cdd9f7ad45e22d4689b293b
TDesjardins <dejavue88@googlemail.com>
2016-02-14 21:49:18 +0100

    Add missing style interfaces

---
+++ gwt-ol3-client/src/main/java/ol/style/RegularShape.java
@@ -0,0 +1,13 @@
+package ol.style;
+
+import com.google.gwt.core.client.js.JsType;
+
+/**
+ * Set regular shape style for vector features.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(prototype = "ol.style.RegularShape")
+public interface RegularShape extends Image {
+}
