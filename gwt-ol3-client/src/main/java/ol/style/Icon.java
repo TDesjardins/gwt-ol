caece4a1739295ddd9535334e027f0a796fa212f
TDesjardins <dejavue88@googlemail.com>
2016-02-14 21:49:18 +0100

    Add missing style interfaces

---
+++ gwt-ol3-client/src/main/java/ol/style/Icon.java
@@ -0,0 +1,13 @@
+package ol.style;
+
+import com.google.gwt.core.client.js.JsType;
+
+/**
+ * Set icon style for vector features.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(prototype = "ol.style.Icon")
+public interface Icon extends Image {
+}
