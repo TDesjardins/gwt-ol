cf52adc3fcdac59c5333d783089c546202309cd4
TDesjardins <dejavue88@googlemail.com>
2016-02-14 22:22:18 +0100

    Add formats

---
+++ gwt-ol3-client/src/main/java/ol/format/Feature.java
@@ -0,0 +1,12 @@
+package ol.format;
+
+import com.google.gwt.core.client.js.JsType;
+
+/**
+ * Feature format.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(prototype = "ol.format.Feature")
+public interface Feature {}
