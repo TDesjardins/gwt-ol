f8a790307764b1652296d337b63416ee4d56dacf
TDesjardins <dejavue88@googlemail.com>
2016-02-14 22:22:18 +0100

    Add formats

---
+++ gwt-ol3-client/src/main/java/ol/format/GPX.java
@@ -0,0 +1,12 @@
+package ol.format;
+
+import com.google.gwt.core.client.js.JsType;
+
+/**
+ * GPX format.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(prototype = "ol.format.GPX")
+public interface GPX extends XMLFeature {}
---
+++ gwt-ol3-client/src/main/java/ol/format/XMLFeature.java
@@ -0,0 +1,12 @@
+package ol.format;
+
+import com.google.gwt.core.client.js.JsType;
+
+/**
+ * XML feature format.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(prototype = "ol.format.XMLFeature")
+public interface XMLFeature extends Feature {}
