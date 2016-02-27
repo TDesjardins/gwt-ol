dcc187ef7ec2af09ff252bfaedab97c890ffed9a
TDesjardins <dejavue88@googlemail.com>
2016-02-27 15:09:46 +0100

    Switch from OL v3.13.1 to OL v3.14.0
    Conflicts:
    	gwt-ol3-client/src/test/java/GwtOL3Test.gwt.xml

--- gwt-ol3-client/src/test/java/ol/GwtOL3BaseTestCase.java
+++ gwt-ol3-client/src/test/java/ol/GwtOL3BaseTestCase.java
@@ -8,7 +8,7 @@ package ol;
 public class GwtOL3BaseTestCase extends BaseTestCase {
 
     public GwtOL3BaseTestCase() {
-        super("http://openlayers.org/en/v3.13.1/build/ol.js", "GwtOL3Test", 10000);
+        super("http://openlayers.org/en/v3.14.0/build/ol.js", "GwtOL3Test", 10000);
     }
 
 }
