1e8add1f7cbecfe0c63b56bb065f19ec1f8fe61d
AAauAAau <lochmann.tobias@gmail.com>
2016-03-11 11:54:51 +0100

    Add missing GeoJSONExample to GwtOl3Playground
    Conflicts:
    	gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/GwtOL3Playground.java

--- gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/example/GeoJSONExample.java
+++ gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/example/GeoJSONExample.java
@@ -16,78 +16,78 @@ import ol.source.Vector;
 
 /**
  * Example of GeoJSON
+ *
  * @author Tobias Lochmann
  */
-public class GeoJSONExample implements Example
-{
+public class GeoJSONExample implements Example {
 
-	/*
-	 * (non-Javadoc)
-	 * @see de.desjardins.ol3.demo.client.example.Example#show()
-	 */
-	@Override
-	public void show()
-	{
+    /* (non-Javadoc)
+     * @see de.desjardins.ol3.demo.client.example.Example#show() */
+    @Override
+    public void show() {
 
-		// Create Linstring 		
-		Coordinate coordinate1 = OLFactory.createCoordinate(4e6, 2e6);
-		Coordinate coordinate2 = OLFactory.createCoordinate(8e6, -2e6);
-		Coordinate[] coordinates = { coordinate1, coordinate2 };
-		LineString ls = OLFactory.createLineString(coordinates);
-		
-		//Create featrue
-		FeatureOptions fo= OLFactory.createOptions();
-		fo.setGeometry(ls);
-		Feature f=OLFactory.createFeature(fo);
-		GWT.log("Feature: "+f);
-		// Convert Feature to GeoJson 
-		GeoJSON gj = OLFactory.createGeoJSON();
-		GeoJSON geoJSON = gj.writeFeatureObject(f, null);
-		// Convert Features from GeoJSON
-		Feature featureGeoJSON = gj.readFeature(geoJSON, null);
-		// show converted Features		
-		Collection<Feature> lstFeatures = OLFactory.createCollection();
-		lstFeatures.push(featureGeoJSON);
-		VectorOptions vectorSourceOptions = OLFactory.createOptions();
-		vectorSourceOptions.setFeatures(OLFactory.<Feature[], Collection<Feature>> createObject2(lstFeatures));
-		Vector vectorSource = OLFactory.createVectorSource(vectorSourceOptions);
+        // Create Linestring
+        Coordinate coordinate1 = OLFactory.createCoordinate(4e6, 2e6);
+        Coordinate coordinate2 = OLFactory.createCoordinate(8e6, -2e6);
+        Coordinate[] coordinates = { coordinate1, coordinate2 };
+        LineString lineString = OLFactory.createLineString(coordinates);
 
-		VectorLayerOptions vectorLayerOptions = OLFactory.createOptions();
-		vectorLayerOptions.setSource(vectorSource);
-		ol.layer.Vector vectorLayer = OLFactory.createVector(vectorLayerOptions);
+        // Create featrue
+        FeatureOptions featureOptions = OLFactory.createOptions();
+        featureOptions.setGeometry(lineString);
+        Feature feature = OLFactory.createFeature(featureOptions);
+        GWT.log("Feature: " + feature);
 
-		// create a OSM-layer
-		XyzOptions osmSourceOptions = OLFactory.createOptions();
-		Osm osmSource = OLFactory.createOsm(osmSourceOptions);
-		LayerOptions osmLayerOptions = OLFactory.createOptions();
-		osmLayerOptions.setSource(osmSource);
-		Tile osmLayer = OLFactory.createTileLayer(osmLayerOptions);
+        // Convert Feature to GeoJson
+        GeoJSON gj = OLFactory.createGeoJSON();
+        GeoJSON geoJSON = gj.writeFeatureObject(feature, null);
 
-		// create a view
-		View view = OLFactory.createView();
-		Coordinate centerCoordinate = OLFactory.createCoordinate(0, 0);
-		view.setCenter(centerCoordinate);
-		view.setZoom(2);
+        // Convert Features from GeoJSON
+        Feature featureGeoJSON = gj.readFeature(geoJSON, null);
 
-		// create the map
-		MapOptions mapOptions = OLFactory.createOptions();
-		mapOptions.setTarget("map");
-		mapOptions.setView(view);
-		Collection<Base> lstLayer = OLFactory.createCollection();
-		lstLayer.push(osmLayer);
-		lstLayer.push(vectorLayer);
-		mapOptions.setLayers(lstLayer);
-		Map map = OLFactory.createMap(mapOptions);
+        // show converted Features
+        Collection<Feature> lstFeatures = OLFactory.createCollection();
+        lstFeatures.push(featureGeoJSON);
+        VectorOptions vectorSourceOptions = OLFactory.createOptions();
+        vectorSourceOptions.setFeatures(OLFactory.<Feature[], Collection<Feature>> createObject2(lstFeatures));
+        Vector vectorSource = OLFactory.createVectorSource(vectorSourceOptions);
 
-		// add some controls
-		map.addControl(OLFactory.createScaleLine());
-		DemoUtils.addDefaultControls(map.getControls());
+        VectorLayerOptions vectorLayerOptions = OLFactory.createOptions();
+        vectorLayerOptions.setSource(vectorSource);
+        ol.layer.Vector vectorLayer = OLFactory.createVector(vectorLayerOptions);
 
-		// add some interactions
-		map.addInteraction(OLFactory.createKeyboardPan());
-		map.addInteraction(OLFactory.createKeyboardZoom());
-		map.addControl(OLFactory.createRotate());
+        // create a OSM-layer
+        XyzOptions osmSourceOptions = OLFactory.createOptions();
+        Osm osmSource = OLFactory.createOsm(osmSourceOptions);
+        LayerOptions osmLayerOptions = OLFactory.createOptions();
+        osmLayerOptions.setSource(osmSource);
+        Tile osmLayer = OLFactory.createTileLayer(osmLayerOptions);
 
-	}
+        // create a view
+        View view = OLFactory.createView();
+        Coordinate centerCoordinate = OLFactory.createCoordinate(0, 0);
+        view.setCenter(centerCoordinate);
+        view.setZoom(2);
+
+        // create the map
+        MapOptions mapOptions = OLFactory.createOptions();
+        mapOptions.setTarget("map");
+        mapOptions.setView(view);
+        Collection<Base> lstLayer = OLFactory.createCollection();
+        lstLayer.push(osmLayer);
+        lstLayer.push(vectorLayer);
+        mapOptions.setLayers(lstLayer);
+        Map map = OLFactory.createMap(mapOptions);
+
+        // add some controls
+        map.addControl(OLFactory.createScaleLine());
+        DemoUtils.addDefaultControls(map.getControls());
+
+        // add some interactions
+        map.addInteraction(OLFactory.createKeyboardPan());
+        map.addInteraction(OLFactory.createKeyboardZoom());
+        map.addControl(OLFactory.createRotate());
+
+    }
 
 }
