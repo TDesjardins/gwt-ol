c0f33119dc431e4b0b723b6c4ee76b81eb41bd61
Tino Desjardins <tino.desjardins@arcor.de>
2016-04-06 17:56:40 +0200

    Format source
    Organize imports

--- gwt-ol3-client/src/test/java/ol/geom/GeometryCollectionTest.java
+++ gwt-ol3-client/src/test/java/ol/geom/GeometryCollectionTest.java
@@ -1,28 +1,36 @@
 package ol.geom;
 
-import ol.*;
+import ol.BaseTestCase;
+import ol.Coordinate;
+import ol.OLFactory;
 
 /**
  * A test case for {@link GeometryCollection}.
- * 
+ *
  * @author sbaumhekel
  */
 public class GeometryCollectionTest extends BaseTestCase {
 
     public void test() {
-	Geometry[] geoms = new Geometry[] { OLFactory.createPoint(1, 2), OLFactory.createLineString(
-		new Coordinate[] { OLFactory.createCoordinate(1, 2), OLFactory.createCoordinate(2, 3) }) };
-	GeometryCollection col = OLFactory.createGeometryCollection(geoms);
-	assertNotNull(col);
-	Geometry[] geoms2 = col.getGeometries();
-	assertNotNull(geoms2);
-	assertEquals(2, geoms2.length);
-	Geometry g1 = geoms2[0];
-	Geometry g2 = geoms2[1];
-	assertNotNull(g1);
-	assertNotNull(g2);
-	assertEquals(Geometry.POINT, g1.getType());
-	assertEquals(Geometry.LINE_STRING, g2.getType());
+
+        Geometry[] geoms = new Geometry[] { OLFactory.createPoint(1, 2), OLFactory.createLineString(
+                new Coordinate[] { OLFactory.createCoordinate(1, 2), OLFactory.createCoordinate(2, 3) }) };
+        GeometryCollection col = OLFactory.createGeometryCollection(geoms);
+
+        assertNotNull(col);
+
+        Geometry[] geoms2 = col.getGeometries();
+        assertNotNull(geoms2);
+
+        assertEquals(2, geoms2.length);
+
+        Geometry g1 = geoms2[0];
+        Geometry g2 = geoms2[1];
+
+        assertNotNull(g1);
+        assertNotNull(g2);
+        assertEquals(Geometry.POINT, g1.getType());
+        assertEquals(Geometry.LINE_STRING, g2.getType());
     }
 
 }
