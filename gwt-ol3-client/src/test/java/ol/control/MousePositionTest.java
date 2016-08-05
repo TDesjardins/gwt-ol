b23351ba151a9d30f2bfd60d31ade471f56ceec4
Tino Desjardins <tino.desjardins@arcor.de>
2016-08-05 12:39:25 +0200

    Add test for MousePositionOptions

--- gwt-ol3-client/src/test/java/ol/control/MousePositionTest.java
+++ gwt-ol3-client/src/test/java/ol/control/MousePositionTest.java
@@ -1,8 +1,11 @@
 package ol.control;
 
 import ol.BaseTestCase;
+import ol.Coordinate;
 import ol.OLFactory;
+import ol.OLUtil;
 import ol.Object;
+import ol.proj.Projection;
 
 /**
  * 
@@ -19,6 +22,27 @@ public class MousePositionTest extends BaseTestCase {
         assertTrue(mousePosition instanceof Object);
         assertTrue(mousePosition instanceof Control);
         
+    }
+    
+    public void testMousePositionWithOptions() {
+    	
+    	Projection projection = OLUtil.getProjection("EPSG:4326");
+    	String undefinedHtml = "no coordinates";
+    	
+    	MousePositionOptions mousePositionOptions = OLFactory.createOptions();
+    	mousePositionOptions.setCoordinateFormat(Coordinate.createStringXY(5));
+    	mousePositionOptions.setProjection(projection);
+    	mousePositionOptions.setUndefinedHTML(undefinedHtml);
+    	
+    	MousePosition mousePosition = OLFactory.createMousePosition(mousePositionOptions);
+    	
+    	assertNotNull(mousePosition);
+        assertTrue(mousePosition instanceof Object);
+        assertTrue(mousePosition instanceof Control);
+        
+        assertNotNull(mousePosition.getProjection());
+        assertEquals(projection, mousePosition.getProjection());
+    	
     }
 
 }
