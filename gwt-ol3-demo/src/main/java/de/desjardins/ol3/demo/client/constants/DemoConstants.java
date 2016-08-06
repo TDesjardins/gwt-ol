765a59e7fd3ee8d8d279eee9064d6b693bcda996
Tino Desjardins <tino.desjardins@arcor.de>
2016-08-06 10:12:39 +0200

    Add constants for examples

---
+++ gwt-ol3-demo/src/main/java/de/desjardins/ol3/demo/client/constants/DemoConstants.java
@@ -0,0 +1,19 @@
+package de.desjardins.ol3.demo.client.constants;
+
+/**
+ * 
+ * @author Tino Desjardins
+ *
+ */
+public final class DemoConstants {
+    
+	public static final String EPSG_4326 = "EPSG:4326";
+	public static final String EPSG_3857 = "EPSG:3857";
+	
+	public static final String MAP_DIV = "map";
+	
+    private DemoConstants() {
+        throw new AssertionError();
+    }
+    
+}
