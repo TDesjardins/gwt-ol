91316c2b082178a97c0d82455125a16040527439
Tino Desjardins <tino.desjardins@arcor.de>
2016-08-12 08:31:00 +0200

    Small enhancements

--- gwt-ol3-client/src/test/java/ol/layer/LayerGroupTest.java
+++ gwt-ol3-client/src/test/java/ol/layer/LayerGroupTest.java
@@ -1,6 +1,8 @@
 package ol.layer;
 
-import ol.*;
+import ol.BaseTestCase;
+import ol.OLFactory;
+import ol.Observable;
 
 /**
  * A test case for {@link ol.layer.Group}.
@@ -11,19 +13,27 @@ public class LayerGroupTest extends BaseTestCase {
 
     @SuppressWarnings({ "static-method", "javadoc" })
     public void test() {
-        // create empty layer group and ensure it is empty
-        Group g = OLFactory.createLayerGroup(null);
-        ol.Collection<ol.layer.Base> c = g.getLayers();
-        assertEquals(0, c.getLength());
+        
+    	// create empty layer group and ensure it is empty
+        Group layerGroup = OLFactory.createLayerGroup(null);
+        assertNotNull(layerGroup);
+        assertTrue(layerGroup instanceof Observable);
+        assertTrue(layerGroup instanceof Base);
+        
+        ol.Collection<ol.layer.Base> layers = layerGroup.getLayers();
+        assertNotNull(layers);
+        assertEquals(0, layers.getLength());
+        
         // add two layers and ensure they are in there
         Layer v1 = OLFactory.createVector(OLFactory.createLayerOptionsWithSource(OLFactory.createVectorSource()));
         Layer v2 = OLFactory.createVector(OLFactory.createLayerOptionsWithSource(OLFactory.createVectorSource()));
-        ol.Collection<ol.layer.Base> c2 = OLFactory.createCollection();
-        c2.push(v1);
-        c2.push(v2);
-        g.setLayers(c2);
-        c = g.getLayers();
-        assertEquals(2, c.getLength());
+        
+        ol.Collection<ol.layer.Base> newLayers = OLFactory.createCollection();
+        newLayers.push(v1);
+        newLayers.push(v2);
+        layerGroup.setLayers(newLayers);
+        layers = layerGroup.getLayers();
+        assertEquals(2, layers.getLength());
     }
 
 }
