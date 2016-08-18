0718f2ac7e2d1acf83fc22d9dc00953423193844
Tino Desjardins <tino.desjardins@arcor.de>
2016-08-18 10:51:41 +0200

    Remove unnecessary method

--- gwt-ol3-client/src/main/java/ol/source/ImageStatic.java
+++ gwt-ol3-client/src/main/java/ol/source/ImageStatic.java
@@ -2,17 +2,11 @@ package ol.source;
 
 import com.google.gwt.core.client.js.JsType;
 
-import ol.control.Attribution;
-
 /**
- * 
+ *
  * @author Tino Desjardins
  *
  */
 @JsType(prototype = "ol.source.ImageStatic")
-public interface ImageStatic extends Image {
-    
-    void setAttributions (Attribution[] attributions);
-            
-}
+public interface ImageStatic extends Image {}
 
