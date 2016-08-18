9dbccc1b3ebd2e48f343640995462bda6ec008f7
Tino Desjardins <tino.desjardins@arcor.de>
2016-08-18 10:51:41 +0200

    Remove unnecessary method

--- gwt-ol3-client/src/main/java/ol/source/ImageStatic.java
+++ gwt-ol3-client/src/main/java/ol/source/ImageStatic.java
@@ -2,21 +2,17 @@ package ol.source;
 
 import jsinterop.annotations.JsType;
 
-import ol.control.Attribution;
-
 /**
- * 
+ *
  * @author Tino Desjardins
  *
  */
 @JsType(isNative = true)
 public class ImageStatic extends Image {
-    
+
     public ImageStatic() {}
-    
+
     public ImageStatic(ImageStaticOptions imageStaticOptions) {}
-    
-    public native void setAttributions (Attribution[] attributions);
-            
+
 }
 
