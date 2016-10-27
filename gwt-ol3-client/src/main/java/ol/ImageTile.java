7674b14b375178f5be4d2f713f07bcb80a68cf2e
Elena Guandalini <elena.guandalini@tol.bz.it>
2016-10-27 17:01:22 +0200

    Add getImage in ImageTile

--- gwt-ol3-client/src/main/java/ol/ImageTile.java
+++ gwt-ol3-client/src/main/java/ol/ImageTile.java
@@ -1,5 +1,7 @@
 package ol;
 
+import com.google.gwt.dom.client.ImageElement;
+
 import jsinterop.annotations.JsType;
 
 /**
@@ -10,4 +12,5 @@ import jsinterop.annotations.JsType;
 @JsType(isNative = true)
 public class ImageTile extends Tile {
 
+	public native ImageElement getImage();
 }
