a5aac33ae6ff6f1d77d2d3e6dc73d6424e2dbc12
Tino Desjardins <tino.desjardins@arcor.de>
2016-11-07 11:54:01 +0100

    Add interactions DragPan and MouseWheelZoom

---
+++ gwt-ol3-client/src/main/java/ol/interaction/MouseWheelZoom.java
@@ -0,0 +1,13 @@
+package ol.interaction;
+
+import jsinterop.annotations.JsType;
+
+/**
+ *
+ * @author Tino Desjardins
+ *
+ */
+@JsType(isNative = true)
+public class MouseWheelZoom extends Interaction {
+
+}
