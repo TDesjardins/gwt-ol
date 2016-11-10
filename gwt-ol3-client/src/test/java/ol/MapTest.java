f58f1304882a7d74be3b96ef8bd81f406e2fb3e7
Tino Desjardins <tino.desjardins@arcor.de>
2016-11-10 14:09:44 +0100

    Refactor test for map

--- gwt-ol3-client/src/test/java/ol/MapTest.java
+++ gwt-ol3-client/src/test/java/ol/MapTest.java
@@ -8,43 +8,43 @@ import ol.OLFactory;
 import ol.proj.ProjectionOptions;
 
 /**
- * 
+ *
  * @author Tino Desjardins
  *
  */
 public class MapTest extends GwtOL3BaseTestCase {
 
     public void testMapCreation() {
-        
+
         injectUrlAndTest(new TestWithInjection() {
-            
+
             @Override
             public void test() {
-                
+
                 ProjectionOptions projectionOptions = OLFactory.createOptions();
                 projectionOptions.setCode("EPSG:21781");
                 projectionOptions.setUnits("m");
-                
-                Projection projection = OLFactory.createProjection(projectionOptions);
-                
+
+                Projection projection = new Projection(projectionOptions);
+
                 ViewOptions viewOptions = OLFactory.createOptions();
                 viewOptions.setProjection(projection);
-                View view = OLFactory.createView(viewOptions);
+                View view = new View(viewOptions);
 
                 Coordinate centerCoordinate = OLFactory.createCoordinate(660000, 190000);
-                
+
                 view.setCenter(centerCoordinate);
                 view.setZoom(9);
-                
+
                 final MapOptions mapOptions = OLFactory.createOptions();
                 mapOptions.setTarget("map");
                 mapOptions.setView(view);
-                
-                /*Map map = Map.newInstance(mapOptions);
-                assertNotNull(map);*/
-                
+
+                Map map = new Map(mapOptions);
+                assertNotNull(map);
+
             }
-            
+
         });
 
     }
