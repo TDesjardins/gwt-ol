e954367f733ef7eca28bffba50e1e8157ce99ba1
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-11-30 18:13:38 +0100

    Prepare for sonatype releases
    It's important to use SNAPSHOT version so the project not get deployed as a release, used the HEAD-SNAPSHOT strategy which is easier and people using SNAPSHOT version do not need to update the SNAPSHOT version, the will keep in the head forever. This also go perfect with the creative versioning used here ;).
    Renamed parent module to gwt-ol3-parent and client from gwt-ol3-client to gwt-ol3 which is the actual lib name.
    Collapsed permutations, OL3 is a one script lib, and the demo size is just 90k, after collapsing all permutation it get up to 100k which is almost the same. So collapsed.

---
+++ ci/deploy-snapshot.sh
@@ -0,0 +1,7 @@
+if [ "$TRAVIS_REPO_SLUG" == "TDesjardins/gwt-ol3" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  mvn -s ci/settings.xml clean source:jar deploy -Dmaven.test.skip=true -Dinvoker.skip=true
+fi
