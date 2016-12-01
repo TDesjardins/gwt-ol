b95176e20b47a8898338bfbee4c8eae039080aa7
Peter Zanetti <peter.zanetti@tol.bz.it>
2016-12-01 10:38:18 +0100

    Used @JsFunction instead of JavaScriptObject for callbacks in XyzOptions

--- gwt-ol3-client/src/main/java/ol/source/XyzOptions.java
+++ gwt-ol3-client/src/main/java/ol/source/XyzOptions.java
@@ -1,7 +1,5 @@
 package ol.source;
 
-import com.google.gwt.core.client.JavaScriptObject;
-
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 import ol.Size;
@@ -11,24 +9,25 @@ import ol.Size;
  *
  * @author Tino Desjardins
  *
+ * @see http://openlayers.org/en/latest/apidoc/ol.source.XYZ.html
  */
 @JsType(isNative = true)
 public interface XyzOptions extends TileImageOptions {
 
-    /**
-     * Set the optional max zoom level. Default is 18.
-     *
-     * @param maxZoom max zoom
-     */
-    @JsProperty
-    void setMaxZoom(int maxZoom);
+	/**
+	 * Set the optional max zoom level. Default is 18.
+	 *
+	 * @param maxZoom max zoom
+	 */
+	@JsProperty
+	void setMaxZoom(int maxZoom);
 
-    /**
-     * Optional function to get tile URL given a tile coordinate and the
-     * projection. Required if url or urls are not provided.
-     */
-    @JsProperty
-    void setTileUrlFunction(JavaScriptObject tileUrlFunction);
+	/**
+	 * Optional function to get tile URL given a tile coordinate and the
+	 * projection. Required if url or urls are not provided.
+	 */
+	@JsProperty
+	void setTileUrlFunction(TileUrlFunction callback);
 
 	/**
 	 * Optional function to load a tile given a URL. The default is
@@ -36,13 +35,12 @@ public interface XyzOptions extends TileImageOptions {
 	 * function(imageTile, src) { imageTile.getImage().src = src; };
 	 */
 	@JsProperty
-	void setTileLoadFunction(JavaScriptObject tileLoadFunction);
+	void setTileLoadFunction(TileLoadFunction callback);
 
 	/**
 	 * Set the tile size used by the tile service. Default is [256, 256] pixels.
 	 * 
-	 * @param tileSize
-	 *            tile size
+	 * @param tileSize tile size
 	 */
 	@JsProperty
 	void setTileSize(Size tileSize);
