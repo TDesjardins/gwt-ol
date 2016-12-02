f896fc01123623ba0923c009760e3e7a2e4cffa5
Peter Zanetti <peter.zanetti@tol.bz.it>
2016-12-02 14:32:42 +0100

    IsEmpty method from Java Collection

--- gwt-ol3-client/src/main/java/ol/Collection.java
+++ gwt-ol3-client/src/main/java/ol/Collection.java
@@ -2,7 +2,7 @@ package ol;
 
 import javax.annotation.Nullable;
 
-import jsinterop.annotations.JsIgnore;
+import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsType;
 
 /**
@@ -14,50 +14,51 @@ import jsinterop.annotations.JsType;
 @JsType(isNative = true)
 public class Collection<T> extends Object {
 
-	public native void clear();
+    public native void clear();
 
-	public native T[] getArray();
+    public native T[] getArray();
 
-	public native int getLength();
+    public native int getLength();
 
-	public native void insertAt(int index, T element);
+    public native void insertAt(int index, T element);
 
-	/**
-	 * Returns <tt>true</tt> if this collection contains no elements.
-	 *
-	 * @return <tt>true</tt> if this collection contains no elements
-	 */
-	@JsIgnore
-	public boolean isEmpty() {
-		return getLength() > 0;
-	}
+  	/**
+  	 * Returns <tt>true</tt> if this collection contains no elements.
+  	 *
+  	 * @return <tt>true</tt> if this collection contains no elements
+  	 */
+    @JsOverlay
+  	public final boolean isEmpty() {
+  		return getLength() > 0;
+  	}
+    
+    public native T item(int index);
 
-	public native T item(int index);
+    public native void pop();
 
-	public native void pop();
+    public native void push(T element);
 
-	public native void push(T element);
+    /**
+     * Remove the first occurrence of an element from the collection.
+     * 
+     * @param element Element.
+     * @return The removed element or undefined if none found.
+     * @api stable
+     */
+    @Nullable
+    public native T remove(T element);
 
-	/**
-	 * Remove the first occurrence of an element from the collection.
-	 * 
-	 * @param element Element.
-	 * @return The removed element or undefined if none found.
-	 * @api stable
-	 */
-	@Nullable
-	public native T remove(T element);
+    /**
+     * Remove the element at the provided index and return it. Return
+     * `undefined` if the collection does not contain this index.
+     * 
+     * @param index Index.
+     * @return Value.
+     * @api stable
+     */
+    @Nullable
+    public native T removeAt(int index);
 
-	/**
-	 * Remove the element at the provided index and return it. Return `undefined`
-	 * if the collection does not contain this index.
-	 * 
-	 * @param index Index.
-	 * @return Value.
-	 * @api stable
-	 */
-	@Nullable
-	public native T removeAt(int index);
+    public native void setAt(int index, T element);
 
-	public native void setAt(int index, T element);
 }
