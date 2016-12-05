dd010a67071037060c3c00823fa71762e51c898e
Peter Zanetti <peter.zanetti@tol.bz.it>
2016-12-05 14:51:57 +0100

    Enumeration for ol.interaction  events added

---
+++ gwt-ol3-client/src/main/java/ol/interaction/DrawEventType.java
@@ -0,0 +1,24 @@
+package ol.interaction;
+
+/**
+ * The draw event type.
+ *
+ * @author Peter Zanetti
+ * 
+ * @see https://github.com/openlayers/ol3/blob/master/src/ol/interaction/draw.js
+ *
+ */
+public enum DrawEventType {
+  /**
+   * Triggered upon feature draw start
+   * 
+   * @see ol.interaction.Draw.Event#drawstart
+   */
+		drawstart,
+	  /**
+	   * Triggered upon feature draw end
+	   * 
+	   * @see ol.interaction.Draw.Event#drawend
+	   */
+		drawend
+}
---
+++ gwt-ol3-client/src/main/java/ol/interaction/DrawMode.java
@@ -0,0 +1,16 @@
+package ol.interaction;
+
+/**
+ *   Draw mode.  This collapses multi-part geometry types with their single-part
+ *
+ * @author Peter Zanetti
+ * 
+ * https://github.com/openlayers/ol3/blob/master/src/ol/interaction/draw.js
+ *
+ */
+public enum DrawMode {
+	Point,
+	LineString,
+	Polygon,
+	Circle	
+}
