06159af585fc7eb8ca0931242b523e9eab5c8b51
Peter Zanetti <peter.zanetti@tol.bz.it>
2016-12-06 08:58:38 +0100

    @JsType for DrawMode

--- gwt-ol3-client/src/main/java/ol/interaction/DrawMode.java
+++ gwt-ol3-client/src/main/java/ol/interaction/DrawMode.java
@@ -1,5 +1,7 @@
 package ol.interaction;
 
+import jsinterop.annotations.JsType;
+
 /**
  *   Draw mode.  This collapses multi-part geometry types with their single-part
  *
@@ -8,6 +10,7 @@ package ol.interaction;
  * https://github.com/openlayers/ol3/blob/master/src/ol/interaction/draw.js
  *
  */
+@JsType
 public enum DrawMode {
 	Point,
 	LineString,
