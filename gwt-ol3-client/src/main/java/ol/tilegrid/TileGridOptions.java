bdc5843da0170ca1d03bbf07c0dfa04cb013ff8d
Tino Desjardins <tino.desjardins@arcor.de>
2017-01-04 10:09:35 +0100

    Make options creatable with new operator

--- gwt-ol3-client/src/main/java/ol/tilegrid/TileGridOptions.java
+++ gwt-ol3-client/src/main/java/ol/tilegrid/TileGridOptions.java
@@ -1,5 +1,6 @@
 package ol.tilegrid;
 
+import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
@@ -13,8 +14,8 @@ import ol.Size;
  *
  * @author Tino Desjardins
  */
-@JsType(isNative = true)
-public interface TileGridOptions extends Options {
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class TileGridOptions implements Options {
 
     /**
      * Set the Extent for the tile grid. No tiles outside this extent will be
@@ -24,7 +25,7 @@ public interface TileGridOptions extends Options {
      * @param extent {@link Extent}
      */
     @JsProperty
-    void setExtent(Extent extent);
+    public native void setExtent(Extent extent);
 
     /**
      * Set the Maximum zoom. Default is based on resolutions array.
@@ -32,7 +33,7 @@ public interface TileGridOptions extends Options {
      * @param maxZoom maximum zoom
      */
     @JsProperty
-    void setMaxZoom(int maxZoom);
+    public native void setMaxZoom(int maxZoom);
 
     /**
      * Set the Minimum zoom. Default is 0.
@@ -40,17 +41,17 @@ public interface TileGridOptions extends Options {
      * @param minZoom minimum zoom
      */
     @JsProperty
-    void setMinZoom(int minZoom);
+    public native void setMinZoom(int minZoom);
 
     /**
      * Set the tile grid origin, i.e. where the x and y axes meet ([z, 0, 0]).
      * Tile coordinates increase left to right and upwards. If not specified,
      * extent or origins must be provided.
-     * 
+     *
      * @param coordinate {@link Coordinate}
      */
     @JsProperty
-    void setOrigin(Coordinate coordinate);
+    public native void setOrigin(Coordinate coordinate);
 
     /**
      * Set the tile grid origins, i.e. where the x and y axes meet ([z, 0, 0]),
@@ -58,38 +59,38 @@ public interface TileGridOptions extends Options {
      * of the resolutions array, i.e. each resolution can have a different
      * origin. Tile coordinates increase left to right and upwards. If not
      * specified, extent or origin must be provided.
-     * 
+     *
      * @param origins origins
      */
     @JsProperty
-    void setOrigins(Coordinate[] origins);
+    public native void setOrigins(Coordinate[] origins);
 
     /**
      * Set resolutions. The array index of each resolution needs to match the
      * zoom level. This means that even if a minZoom is configured, the
      * resolutions array will have a length of maxZoom + 1. Required.
-     * 
+     *
      * @param resolutions resolutions
      */
     @JsProperty
-    void setResolutions(double[] resolutions);
+    public native void setResolutions(double[] resolutions);
 
     /**
      * Set the tile size. Default is [256, 256].
-     * 
+     *
      * @param tileSize tile size
      */
     @JsProperty
-    void setTileSize(Size tileSize);
+    public native void setTileSize(Size tileSize);
 
     /**
      * Set the tile sizes. If given, the array length should match the length of
      * the resolutions array, i.e. each resolution can have a different tile
      * size.
-     * 
+     *
      * @param tileSizes tile sizes
      */
     @JsProperty
-    void setTileSizes(Size[] tileSizes);
+    public native void setTileSizes(Size[] tileSizes);
 
 }
