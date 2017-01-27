2a0753c1b2832dda4a47d8346797cf2826c694f0
Peter Zanetti <peter.zanetti@tol.bz.it>
2017-01-27 16:46:17 +0100

    Add ol.State

---
+++ gwt-ol3-client/src/main/java/ol/source/State.java
@@ -0,0 +1,31 @@
+package ol.source;
+
+import ol.Constant;
+
+/**
+ * State of the source, one of 'undefined', 'loading', 'ready' or 'error'.
+ * 
+ * @author Peter Zanetti
+ */
+public enum State implements Constant<String> {
+		error("error"),
+		loading("loading"),
+		ready("ready"),
+		undefined("undefined");
+
+	private final String value;
+
+	/**
+	 * Constructs a(n) {@link State} object.
+	 *
+	 * @param value
+	 */
+	State(final String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+}
