c71ca16893192d0e1f2d2f29b627d657d72c4eb5
Peter Zanetti <peter.zanetti@tol.bz.it>
2017-01-27 16:49:09 +0100

    TileOptions Hierarchy changed

--- gwt-ol3-client/src/main/java/ol/source/TileWMSOptions.java
+++ gwt-ol3-client/src/main/java/ol/source/TileWMSOptions.java
@@ -6,52 +6,55 @@
  */
 package ol.source;
 
+import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
-import ol.Options;
-import ol.proj.Projection;
+import tol.j2cl.elem.global.Map;
+import tol.j2cl.elem.util.Constant;
 
 /**
  *
  */
 @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
-public class TileWMSOptions implements Options{
+public class TileWMSOptions extends TileImageOptions {
 
-    /**
-     *
-     * @param url
-     */
-    @JsProperty
-    public native void setUrl(String url);
+	@JsProperty
+	public native void setCrossOrigin(String crossOrigin);
 
-    /**
-     *
-     * @param serverType
-     */
-    @JsProperty
-    public native void setServerType(String serverType);
+	@JsProperty
+	public native String getCrossOrigin();
 
-    /**
-     *
-     * @param crossOrigin
-     */
-    @JsProperty
-    public native void setCrossOrigin(String crossOrigin);
+	@JsProperty(name = "serverType")
+	public native String getServerTypeString();
 
-    /**
-     *
-     * @param tileWMSParams
-     *            params for WMS-requests
-     */
-    @JsProperty
-    public native void setParams(TileWMSParams tileWMSParams);
+	@JsProperty(name = "serverType")
+	public native void setServerTypeString(String serverType);
 
-    /**
-     *
-     * @param projection
-     */
-    @JsProperty
-    public native void setProjection(Projection projection);
+	@JsOverlay
+	public WMSServerType getServerType() {
+		return Constant.of(WMSServerType.class, getServerTypeString());
+	}
+
+	@JsOverlay
+	public void setServerType(WMSServerType serverType) {
+		setServerTypeString(serverType.getValue());
+	}
+
+	/**
+	 *
+	 * @param tileWMSParams params for WMS-requests
+	 */
+	@JsProperty(name = "params")
+	public native void setParams(TileWMSParams params);
+
+	@JsProperty(name = "params")
+	public native void setParams(Map<String> params);
+
+	@JsProperty(name = "params")
+	public native TileWMSParams getTileWMSParams();
+
+	@JsProperty(name = "params")
+	public native Map<String> getParams();
 
 }
