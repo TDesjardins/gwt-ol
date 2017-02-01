df6556c5cd0838c45635b6aae837d8a0a6da3b56
Peter Zanetti <peter.zanetti@tol.bz.it>
2017-02-01 09:54:17 +0100

    Add missing ViewOptions properties

--- gwt-ol3-client/src/main/java/ol/ViewOptions.java
+++ gwt-ol3-client/src/main/java/ol/ViewOptions.java
@@ -14,18 +14,41 @@ import ol.proj.Projection;
 @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
 public class ViewOptions implements Options {
 
-    @JsProperty
-    public native void setCenter(Coordinate coordinate);
-
-    @JsProperty
-    public native void setProjection(Projection projection);
-
-    @JsProperty
-    public native void setZoom(double zoom);
-
-    @JsProperty
-    public native void setMinZoom(double minZoom);
-
-    @JsProperty
-    public native void setMaxZoom(double maxZoom);
+		@JsProperty
+		public native void setCenter(Coordinate coordinate);
+	
+		@JsProperty
+		public native void setExtent(Extent extent);
+	
+		@JsProperty
+		public native void setMaxResolution(double maxResolution);
+	
+		@JsProperty
+		public native void setMinResolution(double minResolution);
+	
+		@JsProperty
+		public native void setMaxZoom(double maxZoom);
+	
+		@JsProperty
+		public native void setMinZoom(double minZoom);
+	
+		@JsProperty
+		public native void setProjection(Projection projection);
+	
+		/**
+		 * {@link #setProjection}
+		 *
+		 * @param srsCode combination of authority and identifier such as "EPSG:4326"
+		 */
+		@JsProperty
+		public native void setProjection(String srsCode);
+	
+		@JsProperty
+		public native void setResolutions(double[] resolutions);
+	
+		@JsProperty
+		public native void setZoom(double zoom);
+	
+		@JsProperty
+		public native void setZoomFactor(double zoomFactor);
 }
