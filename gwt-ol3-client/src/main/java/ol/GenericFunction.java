5a9fc63223d031d907c38eb098f071f3600096dc
Tino Desjardins <tino.desjardins@arcor.de>
2017-02-19 08:44:27 +0100

    Add license header

--- gwt-ol3-client/src/main/java/ol/GenericFunction.java
+++ gwt-ol3-client/src/main/java/ol/GenericFunction.java
@@ -1,16 +1,25 @@
-/**
+/*******************************************************************************
+ * Copyright 2014, 2016 gwt-ol3
  *
- * @author mribeiro
- * @date 07/06/16 16:45
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- */
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package ol;
 
 import jsinterop.annotations.JsFunction;
 
 /**
  * @author mribeiro
- *         19-05-2016.
+ *
  */
 @JsFunction
 public interface GenericFunction<T, E> {
