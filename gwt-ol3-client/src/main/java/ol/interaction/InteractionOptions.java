5026823032d087029bfa7f2d360325cfcf57e345
Sebastian Baumhekel <sebastian.baumhekel@gmail.com>
2017-04-28 13:20:17 +0200

    Adapt ol.interactions to OpenLayers 4.10. Allow defining options for default interactions (to be used in ol.Map creation). Add functions for min/max zoom on view, which were integrated in the last OpenLayers 4 releases.

---
+++ gwt-ol3-client/src/main/java/ol/interaction/InteractionOptions.java
@@ -0,0 +1,93 @@
+package ol.interaction;
+
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import ol.Options;
+
+/**
+ * Defaults options for {@link Interaction#defaults(InteractionOptions)}.
+ * @author sbaumhekel
+ */
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class InteractionOptions implements Options {
+
+    /**
+     * Whether Alt-Shift-drag rotate is desired. Default is true.
+     * @param altShiftDragRotate
+     */
+    @JsProperty
+    public native void setAltShiftDragRotate(boolean altShiftDragRotate);
+
+    /**
+     * Zoom to the closest integer zoom level after the wheel/trackpad or pinch
+     * gesture ends. Default is false.
+     * @param constrainResolution
+     */
+    @JsProperty
+    public native void setConstrainResolution(boolean constrainResolution);
+
+    /**
+     * Whether double click zoom is desired. Default is true.
+     * @param doubleClickZoom
+     */
+    @JsProperty
+    public native void setDoubleClickZoom(boolean doubleClickZoom);
+
+    /**
+     * Whether drag pan is desired. Default is true.
+     * @param dragPan
+     */
+    @JsProperty
+    public native void setDragPan(boolean dragPan);
+
+    /**
+     * Whether keyboard interaction is desired. Default is true.
+     * @param keyboard
+     */
+    @JsProperty
+    public native void setKeyboard(boolean keyboard);
+
+    /**
+     * Whether mousewheel zoom is desired. Default is true.
+     * @param mouseWheelZoom
+     */
+    @JsProperty
+    public native void setMouseWheelZoom(boolean mouseWheelZoom);
+
+    /**
+     * Whether pinch rotate is desired. Default is true.
+     * @param pinchRotate
+     */
+    @JsProperty
+    public native void setPinchRotate(boolean pinchRotate);
+
+    /**
+     * Whether pinch zoom is desired. Default is true.
+     * @param pinchZoom
+     */
+    @JsProperty
+    public native void setPinchZoom(boolean pinchZoom);
+
+    /**
+     * Whether Shift-drag zoom is desired. Default is true.
+     * @param shiftDragZoom
+     */
+    @JsProperty
+    public native void setShiftDragZoom(boolean shiftDragZoom);
+
+    /**
+     * Zoom delta.
+     * @param zoomDelta
+     */
+    @JsProperty
+    public native void setZoomDelta(int zoomDelta);
+
+    /**
+     * Zoom duration.
+     * @param zoomDuration
+     */
+    @JsProperty
+    public native void setZoomDuration(int zoomDuration);
+
+}
