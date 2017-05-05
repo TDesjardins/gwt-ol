8ce9046b3a79fcaac344351313dad866e69bb453
Tino Desjardins <tino.desjardins@arcor.de>
2017-05-05 14:28:23 +0200

    Format code
    Fix doc

--- gwt-ol3-demo/src/main/java/com/github/tdesjardins/ol3/demo/client/example/ClusterExample.java
+++ gwt-ol3-demo/src/main/java/com/github/tdesjardins/ol3/demo/client/example/ClusterExample.java
@@ -1,19 +1,18 @@
 /*******************************************************************************
  * Copyright 2014, 2017 gwt-ol3
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
-/**
- *
- */
 package com.github.tdesjardins.ol3.demo.client.example;
 
 import java.util.ArrayList;
@@ -51,111 +50,108 @@ import ol.style.StyleOptions;
 import ol.style.TextOptions;
 
 /**
- * Example about using markers.
+ * Example about using clusters.
  *
- * @author Tino Desjardins
+ * @author mazlixek
  */
 public class ClusterExample implements Example {
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see de.desjardins.ol3.demo.client.example.Example#show()
-   */
-  @Override
-  public void show(String exampleId) {
+    /* (non-Javadoc)
+     * @see de.desjardins.ol3.demo.client.example.Example#show() */
+    @Override
+    public void show(String exampleId) {
 
-    Collection<Feature> features = new Collection<Feature>();
-    int e = 450000;
-    for (int i = 0; i < 20000; ++i) {
-      // create a point
-      Coordinate coordinate =
-          OLFactory.createCoordinate(2 * e * Math.random() - e, 2 * e * Math.random() - e);
-      Point point = new Point(coordinate);
+        Collection<Feature> features = new Collection<Feature>();
+        int e = 450000;
+        
+        for (int i = 0; i < 20000; ++i) {
+            // create a point
+            Coordinate coordinate = OLFactory.createCoordinate(2 * e * Math.random() - e, 2 * e * Math.random() - e);
+            Point point = new Point(coordinate);
 
-      // create feature
-      FeatureOptions featureOptions = OLFactory.createOptions();
-      featureOptions.setGeometry(point);
-      Feature feature = new Feature(featureOptions);
+            // create feature
+            FeatureOptions featureOptions = OLFactory.createOptions();
+            featureOptions.setGeometry(point);
+            Feature feature = new Feature(featureOptions);
 
-      features.push(feature);
+            features.push(feature);
+        }
+
+        // create source
+        VectorOptions vectorSourceOptions = OLFactory.createOptions();
+        vectorSourceOptions.setFeatures(features);
+        Vector vectorSource = new Vector(vectorSourceOptions);
+
+        // create clustering
+        ClusterOptions clusterOptions = new ClusterOptions();
+        clusterOptions.setDistance(40);
+        clusterOptions.setSource(vectorSource);
+        Cluster clusterSource = new Cluster(clusterOptions);
+
+        // create vector layer
+        VectorLayerOptions vectorLayerOptions = OLFactory.createOptions();
+        vectorLayerOptions.setSource(clusterSource);
+        vectorLayerOptions.setStyle(new GenericFunction<Feature, Style[]>() {
+
+            @Override
+            public Style[] call(Feature cluster) {
+                // create style
+                List<Style> style = new ArrayList<>();
+
+                Feature[] features = (Feature[])cluster.get("features");
+                int clusterSize = features.length;
+
+                StyleOptions styleOptions = new StyleOptions();
+
+                TextOptions textOptions = OLFactory.createTextOptions();
+                textOptions.setText(String.valueOf(clusterSize));
+                styleOptions.setText(OLFactory.createText(textOptions));
+
+                CircleOptions circleOptions = new CircleOptions();
+                circleOptions.setRadius(10);
+                circleOptions.setFill(OLFactory.createFill(OLFactory.createColor("#3399CC")));
+                styleOptions.setImage(OLFactory.createCircleStyle(circleOptions));
+                style.add(new Style(styleOptions));
+
+                return style.toArray(new Style[0]);
+            }
+        });
+
+        ol.layer.Vector vectorLayer = new ol.layer.Vector(vectorLayerOptions);
+
+        // create a OSM-layer
+        XyzOptions osmSourceOptions = OLFactory.createOptions();
+        Osm osmSource = new Osm(osmSourceOptions);
+
+        LayerOptions osmLayerOptions = OLFactory.createOptions();
+        osmLayerOptions.setSource(osmSource);
+
+        Tile osmLayer = new Tile(osmLayerOptions);
+
+        // create a view
+        View view = new View();
+        Coordinate centerCoordinate = OLFactory.createCoordinate(0, 0);
+        view.setCenter(centerCoordinate);
+        view.setZoom(2);
+
+        // create the map
+        MapOptions mapOptions = new MapOptions();
+        mapOptions.setTarget(exampleId);
+        mapOptions.setView(view);
+        Collection<Base> lstLayer = new Collection<Base>();
+        lstLayer.push(osmLayer);
+        lstLayer.push(vectorLayer);
+        mapOptions.setLayers(lstLayer);
+        Map map = new Map(mapOptions);
+
+        // add some controls
+        map.addControl(new ScaleLine());
+        DemoUtils.addDefaultControls(map.getControls());
+
+        // add some interactions
+        map.addInteraction(new KeyboardPan());
+        map.addInteraction(new KeyboardZoom());
+        map.addControl(new Rotate());
     }
-
-
-    // create source
-    VectorOptions vectorSourceOptions = OLFactory.createOptions();
-    vectorSourceOptions.setFeatures(features);
-    Vector vectorSource = new Vector(vectorSourceOptions);
-
-    // create clustering
-    ClusterOptions clusterOptions = new ClusterOptions();
-    clusterOptions.setDistance(40);
-    clusterOptions.setSource(vectorSource);
-    Cluster clusterSource = new Cluster(clusterOptions);
-
-    // create vector layer
-    VectorLayerOptions vectorLayerOptions = OLFactory.createOptions();
-    vectorLayerOptions.setSource(clusterSource);
-    vectorLayerOptions.setStyle(new GenericFunction<Feature, Style[]>() {
-
-      @Override
-      public Style[] call(Feature cluster) {
-        // create style
-        List<Style> style = new ArrayList<>();
-
-        Feature[] features = (Feature[]) cluster.get("features");
-        int clusterSize = features.length;
-
-        StyleOptions styleOptions = new StyleOptions();
-
-        TextOptions textOptions = OLFactory.createTextOptions();
-        textOptions.setText(String.valueOf(clusterSize));
-        styleOptions.setText(OLFactory.createText(textOptions));
-
-        CircleOptions circleOptions = new CircleOptions();
-        circleOptions.setRadius(10);
-        circleOptions.setFill(OLFactory.createFill(OLFactory.createColor("#3399CC")));
-        styleOptions.setImage(OLFactory.createCircleStyle(circleOptions));
-        style.add(new Style(styleOptions));
-
-        return style.toArray(new Style[0]);
-      }
-    });
-
-    ol.layer.Vector vectorLayer = new ol.layer.Vector(vectorLayerOptions);
-
-    // create a OSM-layer
-    XyzOptions osmSourceOptions = OLFactory.createOptions();
-    Osm osmSource = new Osm(osmSourceOptions);
-
-    LayerOptions osmLayerOptions = OLFactory.createOptions();
-    osmLayerOptions.setSource(osmSource);
-
-    Tile osmLayer = new Tile(osmLayerOptions);
-
-    // create a view
-    View view = new View();
-    Coordinate centerCoordinate = OLFactory.createCoordinate(0, 0);
-    view.setCenter(centerCoordinate);
-    view.setZoom(2);
-
-    // create the map
-    MapOptions mapOptions = new MapOptions();
-    mapOptions.setTarget(exampleId);
-    mapOptions.setView(view);
-    Collection<Base> lstLayer = new Collection<Base>();
-    lstLayer.push(osmLayer);
-    lstLayer.push(vectorLayer);
-    mapOptions.setLayers(lstLayer);
-    Map map = new Map(mapOptions);
-
-    // add some controls
-    map.addControl(new ScaleLine());
-    DemoUtils.addDefaultControls(map.getControls());
-
-    // add some interactions
-    map.addInteraction(new KeyboardPan());
-    map.addInteraction(new KeyboardZoom());
-    map.addControl(new Rotate());
-  }
+    
 }
