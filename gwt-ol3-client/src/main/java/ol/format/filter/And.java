3f8e4b78eacd0cc5d2a791e0c9a0e6943eb185e0
Tino Desjardins <tino.desjardins@arcor.de>
2017-07-21 18:46:58 +0200

    Add filters

---
+++ gwt-ol3-client/src/main/java/ol/format/filter/And.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright 2014, 2016 gwt-ol3
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package ol.format.filter;
+
+import jsinterop.annotations.JsType;
+
+/**
+ * Represents a logical <And> operator 
+ * between two filter conditions.
+ * 
+ * @author hmgn
+ *
+ */
+@JsType(isNative = true)
+public class And extends LogicalBinary {
+
+	public And(Filter conditionA, Filter conditionB) {
+		super("And", conditionA, conditionB);
+	}
+
+}
