9d1777b30c414ce14ec0bbfc5075c3cb64af64cc
Tino Desjardins <tino.desjardins@arcor.de>
2017-08-27 08:39:52 +0200

    Add ComparisonBinary filters

---
+++ gwt-ol3-client/src/main/java/ol/format/filter/EqualTo.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright 2014, 2017 gwt-ol3
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package ol.format.filter;
+
+import jsinterop.annotations.JsType;
+
+/**
+ * Represents a <PropertyIsEqualTo> comparison operator.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(isNative = true)
+public class EqualTo extends ComparisonBinary {
+
+    public EqualTo(String propertyName, String expression) {
+        super("PropertyIsEqualTo", propertyName, expression);
+    }
+    
+    public EqualTo(String propertyName, String expression, boolean matchCase) {
+        super("PropertyIsEqualTo", propertyName, expression, matchCase);
+    }
+    
+    public EqualTo(String propertyName, Number expression) {
+        super("PropertyIsEqualTo", propertyName, expression);
+    }
+    
+    public EqualTo(String propertyName, Number expression, boolean matchCase) {
+        super("PropertyIsEqualTo", propertyName, expression, matchCase);
+    }
+	
+}
---
+++ gwt-ol3-client/src/main/java/ol/format/filter/NotEqualTo.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright 2014, 2017 gwt-ol3
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package ol.format.filter;
+
+import jsinterop.annotations.JsType;
+
+/**
+ * Represents a <PropertyIsNotEqualTo> comparison operator.
+ * 
+ * @author Tino Desjardins
+ *
+ */
+@JsType(isNative = true)
+public class NotEqualTo extends ComparisonBinary {
+
+    public NotEqualTo(String propertyName, String expression) {
+        super("PropertyIsNotEqualTo", propertyName, expression);
+    }
+    
+    public NotEqualTo(String propertyName, String expression, boolean matchCase) {
+        super("PropertyIsNotEqualTo", propertyName, expression, matchCase);
+    }
+    
+    public NotEqualTo(String propertyName, Number expression) {
+        super("PropertyIsNotEqualTo", propertyName, expression);
+    }
+    
+    public NotEqualTo(String propertyName, Number expression, boolean matchCase) {
+        super("PropertyIsNotEqualTo", propertyName, expression, matchCase);
+    }
+	
+}
