284111c27411ed608ebb3d3a3be11e987894ed49
Tino Desjardins <tino.desjardins@arcor.de>
2017-11-19 11:43:23 +0100

    Moving array like class Color from JSNI to JsInterop

--- gwt-ol3-client/src/main/java/ol/color/Color.java
+++ gwt-ol3-client/src/main/java/ol/color/Color.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014, 2015 gwt-ol3
+ * Copyright 2014, 2017 gwt-ol3
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,10 @@
  *******************************************************************************/
 package ol.color;
 
-import com.google.gwt.core.client.JavaScriptObject;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+import jsinterop.base.JsArrayLike;
 
 /**
  * A color represented as a short array [red, green, blue, alpha]. red, green,
@@ -24,46 +27,55 @@ import com.google.gwt.core.client.JavaScriptObject;
  *
  * @author sbaumhekel
  */
-public class Color extends JavaScriptObject {
+@JsType(isNative = true, name = "Array", namespace = JsPackage.GLOBAL)
+public class Color implements JsArrayLike<Double> {
 
-    @Deprecated
-    protected Color() {
-    }
+    /**
+     * @param red red (0-255)
+     * @param green green (0-255)
+     * @param blue blue (0-255)
+     * @param alpha alpha (0-1), 1 is solid
+     */
+    public Color(int red, int green, int blue, float alpha) {}
 
     /**
      * Gets the alpha value.
      *
      * @return alpha value (0-1)
      */
-    public final native float getAlpha() /*-{
-		return this[3];
-    }-*/;
+    @JsOverlay
+    public final float getAlpha() {
+        return this.getAt(3).floatValue();
+    };
 
     /**
      * Gets the blue value.
      *
      * @return blue value (0-255)
      */
-    public final native int getBlue() /*-{
-		return this[2];
-    }-*/;
+    @JsOverlay
+    public final int getBlue() {
+        return this.getAt(2).intValue();
+    };
 
     /**
      * Gets the green value.
      *
      * @return green value (0-255)
      */
-    public final native int getGreen() /*-{
-		return this[1];
-    }-*/;
+    @JsOverlay
+    public final int getGreen() {
+        return this.getAt(1).intValue();
+    };
 
     /**
      * Gets the red value.
      *
      * @return red value (0-255)
      */
-    public final native int getRed() /*-{
-		return this[0];
-    }-*/;
+    @JsOverlay
+    public final int getRed() {
+        return this.getAt(0).intValue();
+    };
 
 }
