fdea228fd72a9b26df9c22d6432e13f60bf67cbf
Tino Desjardins <tino.desjardins@arcor.de>
2017-11-25 10:38:58 +0100

    Add constructor with geometry parameter

--- gwt-ol3-client/src/main/java/ol/Feature.java
+++ gwt-ol3-client/src/main/java/ol/Feature.java
@@ -57,11 +57,12 @@ import ol.style.Style;
 @JsType(isNative = true)
 public class Feature extends Object {
 
-    
     public Feature() {}
-    
+
     public Feature(FeatureOptions featureOptions) {}
-    
+
+    public Feature(Geometry geometry) {}
+
     /**
      * Clone this feature. If the original feature has a geometry it is also
      * cloned. The feature id is not set in the clone.
