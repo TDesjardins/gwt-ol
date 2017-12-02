ecc062436c6fc4a83e3ba25929da13bcd708bc22
Tino Desjardins <tino.desjardins@arcor.de>
2017-12-02 11:57:43 +0100

    Remove white spaces

--- gwt-ol3-client/src/main/java/ol/tilegrid/TileGrid.java
+++ gwt-ol3-client/src/main/java/ol/tilegrid/TileGrid.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014, 2016 gwt-ol3
+ * Copyright 2014, 2017 gwt-ol3
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,9 +32,9 @@ import ol.Size;
 public class TileGrid {
 
     public TileGrid() {}
-    
+
     public TileGrid(TileGridOptions tileGridOptions) {}
-    
+
     /**
      * Get the extent for this tile grid, if it was configured.
      * 
