90d5961d35301376328385975b3f54959830307c
Tino Desjardins <tino.desjardins@arcor.de>
2017-12-08 13:54:03 +0100

    Update OL from v4.6.2 to v4.6.3

--- gwt-ol3-client/src/test/java/ol/GwtOL3BaseTestCase.java
+++ gwt-ol3-client/src/test/java/ol/GwtOL3BaseTestCase.java
@@ -23,7 +23,7 @@ package ol;
 public abstract class GwtOL3BaseTestCase extends BaseTestCase {
 
     public GwtOL3BaseTestCase() {
-        super("http://openlayers.org/en/v4.6.2/build/ol.js", "ol.GwtOL3Test", 10000);
+        super("http://openlayers.org/en/v4.6.3/build/ol.js", "ol.GwtOL3Test", 10000);
     }
 
 }
