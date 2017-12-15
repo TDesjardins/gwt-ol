4f4c081450730ecd586c2ea6a26435df0d0819bf
Tino Desjardins <tino.desjardins@arcor.de>
2017-12-15 17:08:01 +0100

    Add documentation

--- gwt-ol3-client/src/main/java/ol/tilegrid/TileGrid.java
+++ gwt-ol3-client/src/main/java/ol/tilegrid/TileGrid.java
@@ -40,33 +40,39 @@ public class TileGrid {
 
     public TileGrid(TileGridOptions tileGridOptions) {}
 
+    /**
+     * Creates a tile grid with a standard XYZ tiling scheme.
+     *
+     * @param tileGridOptions
+     * @return grid with a standard XYZ tiling scheme
+     */
     @JsMethod(name = "createXYZ", namespace = PACKAGE_TILEGRID)
     public static native TileGrid createXyz(TileGridOptions tileGridOptions);
 
     /**
      * Get the extent for this tile grid, if it was configured.
-     * 
+     *
      * @return {ol.Extent} Extent.
      */
     public native Extent getExtent();
 
     /**
      * Get the maximum zoom level for the grid.
-     * 
+     *
      * @return {number} Max zoom.
      */
     public native int getMaxZoom();
 
     /**
      * Get the minimum zoom level for the grid.
-     * 
+     *
      * @return {number} Min zoom.
      */
     public native int getMinZoom();
 
     /**
      * Get the origin for the grid at the given zoom level.
-     * 
+     *
      * @param z
      *            Z.
      * @return {ol.Coordinate} Origin.
@@ -75,7 +81,7 @@ public class TileGrid {
 
     /**
      * Get the resolution for the given zoom level.
-     * 
+     *
      * @param z
      *            Z.
      * @return {number} Resolution.
@@ -84,7 +90,7 @@ public class TileGrid {
 
     /**
      * Get the list of resolutions for the tile grid.
-     * 
+     *
      * @return {Array.<number>} Resolutions.
      */
     public native double[] getResolutions();
@@ -93,7 +99,7 @@ public class TileGrid {
      * Get the tile size for a zoom level. The type of the return value matches
      * the `tileSize` or `tileSizes` that the tile grid was configured with. To
      * always get an `ol.Size`, run the result through `ol.size.toSize()`.
-     * 
+     *
      * @param z
      *            Z.
      * @return {number|ol.Size} Tile size.
