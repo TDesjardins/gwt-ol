be4e7a10d338325f5aec919daef2933281a2fafe
Tino Desjardins <tino.desjardins@arcor.de>
2018-01-12 15:17:07 +0100

    Remove whitespaces

--- gwt-ol3-demo/src/main/java/com/github/tdesjardins/ol3/demo/client/utils/DemoUtils.java
+++ gwt-ol3-demo/src/main/java/com/github/tdesjardins/ol3/demo/client/utils/DemoUtils.java
@@ -34,11 +34,11 @@ import ol.source.XyzOptions;
  *
  */
 public final class DemoUtils {
-    
+
     private DemoUtils() {
         throw new AssertionError();
     }
-    
+
     /**
      * Creates some default controls.
      * 
@@ -61,26 +61,26 @@ public final class DemoUtils {
      * @return test polygon geometry (EPSG:3857)
      */
     public static Polygon createTestPolygon() {
-    	
-    	Coordinate[][] coordinates = new Coordinate[1][4];
-    	
-    	Coordinate point1 = OLFactory.createCoordinate(13.36, 52.53);
-    	Coordinate point2 = OLFactory.createCoordinate(13.36, 52.51);
-    	Coordinate point3 = OLFactory.createCoordinate(13.37, 52.52);
-    	Coordinate point4 = OLFactory.createCoordinate(13.36, 52.53);
-    	
-    	coordinates[0][0] = point1;
-    	coordinates[0][1] = point2;
-    	coordinates[0][2] = point3;
-    	coordinates[0][3] = point4;
-     	
-    	Coordinate[][] tranformedCoordinates = new Coordinate[coordinates.length][];
-    	
-    	tranformedCoordinates[0] = OLUtil.transform(coordinates[0], "EPSG:4326", "EPSG:3857");
-    	return OLFactory.createPolygon(tranformedCoordinates);
+
+        Coordinate[][] coordinates = new Coordinate[1][4];
+
+        Coordinate point1 = OLFactory.createCoordinate(13.36, 52.53);
+        Coordinate point2 = OLFactory.createCoordinate(13.36, 52.51);
+        Coordinate point3 = OLFactory.createCoordinate(13.37, 52.52);
+        Coordinate point4 = OLFactory.createCoordinate(13.36, 52.53);
+
+        coordinates[0][0] = point1;
+        coordinates[0][1] = point2;
+        coordinates[0][2] = point3;
+        coordinates[0][3] = point4;
+
+        Coordinate[][] tranformedCoordinates = new Coordinate[coordinates.length][];
+
+        tranformedCoordinates[0] = OLUtil.transform(coordinates[0], "EPSG:4326", "EPSG:3857");
+        return OLFactory.createPolygon(tranformedCoordinates);
 
     }
-    
+
     public static Base createOsmLayer() {
         XyzOptions osmSourceOptions = OLFactory.createOptions();
 
@@ -90,5 +90,5 @@ public final class DemoUtils {
 
         return new Tile(osmLayerOptions);
     }
-    
+
 }
