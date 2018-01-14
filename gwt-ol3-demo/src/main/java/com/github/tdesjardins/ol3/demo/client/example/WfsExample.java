4cbf51ad2ac9be9343abad0541f7a27f71c90509
Tino Desjardins <tino.desjardins@arcor.de>
2018-01-14 17:10:56 +0100

    Remove whitespaces

--- gwt-ol3-demo/src/main/java/com/github/tdesjardins/ol3/demo/client/example/WfsExample.java
+++ gwt-ol3-demo/src/main/java/com/github/tdesjardins/ol3/demo/client/example/WfsExample.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014, 2016 gwt-ol3
+ * Copyright 2014, 2017 gwt-ol3
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,11 +56,11 @@ public class WfsExample implements Example {
         VectorLayerOptions vectorLayerOptions = new VectorLayerOptions();
         vectorLayerOptions.setSource(vectorSource);
         ol.layer.Vector wfsLayer = new ol.layer.Vector(vectorLayerOptions);
-        
+
         // create a view
         View view = new View();
 
-        Coordinate centerCoordinate = OLFactory.createCoordinate(-8908887.277395891, 5381918.072437216);
+        Coordinate centerCoordinate = new Coordinate(-8908887.277395891, 5381918.072437216);
         view.setCenter(centerCoordinate);
         view.setZoom(12);
         view.setMaxZoom(19);
@@ -74,44 +74,44 @@ public class WfsExample implements Example {
 
         map.addLayer(DemoUtils.createOsmLayer());
         map.addLayer(wfsLayer);
-        
+
         Wfs wfs = new Wfs();
         WfsWriteFeatureOptions wfsWriteFeatureOptions = new WfsWriteFeatureOptions();
-        
+
         String[] featureTypes = {"water_areas"};
         wfsWriteFeatureOptions.setSrsName("EPSG:3857");
         wfsWriteFeatureOptions.setFeaturePrefix("osm");
         wfsWriteFeatureOptions.setFeatureNS("http://openstreemap.org");
         wfsWriteFeatureOptions.setFeatureTypes(featureTypes);
-        
+
         // set a filter
         wfsWriteFeatureOptions.setFilter(new IsLike("name", "Mississippi*"));
         wfsWriteFeatureOptions.setOutputFormat("application/json");
-        
+
         // create WFS-XML node
         Node wfsNode = wfs.writeGetFeature(wfsWriteFeatureOptions);
-        
+
         RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.POST, "https://ahocevar.com/geoserver/wfs");
         requestBuilder.setRequestData(new XMLSerializer().serializeToString(wfsNode));
         requestBuilder.setCallback(new RequestCallback() {
-            
+
             @Override
             public void onResponseReceived(com.google.gwt.http.client.Request request, Response response) {
-                
+
                 GeoJson geoJson = new GeoJson();
                 Feature[] features = geoJson.readFeatures(response.getText());
-                
+
                 vectorSource.addFeatures(features);
                 map.getView().fit(vectorSource.getExtent());
             }
-            
+
             @Override
             public void onError(com.google.gwt.http.client.Request request, Throwable exception) {
                 Window.alert(exception.getMessage());
             }
 
         });
-        
+
         try {
             requestBuilder.send();
         } catch (RequestException e) {
