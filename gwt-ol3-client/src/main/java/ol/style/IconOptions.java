3751a040a7662aa88e337721a892fa9ee5aed00a
es <eric.smets@fks.be>
2018-02-13 12:22:09 +0100

    Add setAnchor function on IconOptions

--- gwt-ol3-client/src/main/java/ol/style/IconOptions.java
+++ gwt-ol3-client/src/main/java/ol/style/IconOptions.java
@@ -91,5 +91,11 @@ public class IconOptions implements Options {
      */
     @JsProperty
     public native void setSrc(String imageUri);
+    
+    /**
+     * @param anchor set Anchor. Default value is [0.5, 0.5] (icon center).
+     */
+    @JsProperty
+    public native void setAnchor(Double[] anchor);
 
 }
