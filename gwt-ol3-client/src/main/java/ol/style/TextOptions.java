dfbaef96c4843a0d597cbb84f74c958985b64a67
es <eric.smets@fks.be>
2018-04-01 14:17:41 +0200

    Add placement value on Text

--- gwt-ol3-client/src/main/java/ol/style/TextOptions.java
+++ gwt-ol3-client/src/main/java/ol/style/TextOptions.java
@@ -99,5 +99,13 @@ public class TextOptions implements Options {
 	 */
 	@JsProperty
 	public native void setTextBaseline(String textBaseline);
+	
+	/**
+	 * Test placement. Possible values: 'point', 'line'. The default value is point
+	 * @param placement String
+	 */
+	@JsProperty
+	public native void setPlacement(String placement);
+
 
 }
