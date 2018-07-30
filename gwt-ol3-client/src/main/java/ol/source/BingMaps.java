3943e70da2b4f1d58d5e9e83922140e5c0d86751
Tino Desjardins <tino.desjardins@arcor.de>
2018-07-30 12:05:15 +0200

    Remove whitespaces

--- gwt-ol3-client/src/main/java/ol/source/BingMaps.java
+++ gwt-ol3-client/src/main/java/ol/source/BingMaps.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014, 2017 gwt-ol3
+ * Copyright 2014, 2018 gwt-ol3
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import jsinterop.annotations.JsType;
 
 /**
  * Layer source for the Bing Maps tile server.
- * 
+ *
  * @author gkresic
  *
  */
@@ -27,11 +27,11 @@ import jsinterop.annotations.JsType;
 public class BingMaps extends TileImage {
 
     public BingMaps() {}
-    
+
     public BingMaps(BingMapsOptions bingSourceOptions) {}
 
     public native String getApiKey();
-    
+
     public native String getImagerySet();
-    
+
 }
