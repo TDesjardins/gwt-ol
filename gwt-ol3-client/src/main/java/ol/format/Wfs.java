978f175808b9591a0f59a1d6abcd4dd448235241
Tino Desjardins <tino.desjardins@arcor.de>
2018-08-21 08:52:42 +0200

    Remove whitespaces

--- gwt-ol3-client/src/main/java/ol/format/Wfs.java
+++ gwt-ol3-client/src/main/java/ol/format/Wfs.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014, 2017 gwt-ol3
+ * Copyright 2014, 2018 gwt-ol3
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,45 +36,45 @@ import javax.annotation.Nullable;
 @JsType(isNative = true, name = "WFS")
 public class Wfs extends XmlFeature {
 
-	public Wfs() {}
-    
+    public Wfs() {}
+
     public Wfs(Options wfsFormatOptions) {}
 
 
-	/**
-	 * Read all features from a WFS FeatureCollection.
-	 * 
-	 * @param source Document | Node | Object | string
-	 * @param readOptions Read options.
-	 * @return [] {@link Feature}
-	 */
-	public native Feature[] readFeatures(java.lang.Object source, @Nullable Options readOptions);
+    /**
+     * Read all features from a WFS FeatureCollection.
+     *
+     * @param source Document | Node | Object | string
+     * @param readOptions Read options.
+     * @return [] {@link Feature}
+     */
+    public native Feature[] readFeatures(java.lang.Object source, @Nullable Options readOptions);
 
-	/**
-	 * Read the projection from a WFS source.
-	 * 
-	 * @param source Document | Node | Object | string
-	 * @return {@link Geometry}
-	 */
-	public native Projection readProjection(java.lang.Object source);
-	
-	/**
-	 * Encode format as WFS GetFeature and return the Node.
-	 * 
-	 * @param writeOptions write options
-	 * @return root node
-	 */
-	public native Node writeGetFeature(WfsWriteFeatureOptions writeOptions);
-	
-	/**
-	 * Encode format as WFS Transaction and return the Node.
-	 * 
-	 * @param inserts The features to insert.
-	 * @param updates The features to update.
-	 * @param deletes The features to delete.
-	 * @param writeOptions write options
-	 * @return root node
-	 */
-	public native Node writeTransaction(Feature[] inserts, Feature[] updates, Feature[] deletes, Options writeOptions);
-    
+    /**
+     * Read the projection from a WFS source.
+     *
+     * @param source Document | Node | Object | string
+     * @return {@link Geometry}
+     */
+    public native Projection readProjection(java.lang.Object source);
+
+    /**
+     * Encode format as WFS GetFeature and return the Node.
+     *
+     * @param writeOptions write options
+     * @return root node
+     */
+    public native Node writeGetFeature(WfsWriteFeatureOptions writeOptions);
+
+    /**
+     * Encode format as WFS Transaction and return the Node.
+     *
+     * @param inserts The features to insert.
+     * @param updates The features to update.
+     * @param deletes The features to delete.
+     * @param writeOptions write options
+     * @return root node
+     */
+    public native Node writeTransaction(Feature[] inserts, Feature[] updates, Feature[] deletes, Options writeOptions);
+
 }
