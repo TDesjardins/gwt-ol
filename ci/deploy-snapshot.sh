76225b630e34aa5ddf1293d14c4f3ae2512358bb
Tino Desjardins <tino.desjardins@arcor.de>
2019-02-13 13:26:06 +0100

    Add output messages

--- ci/deploy-snapshot.sh
+++ ci/deploy-snapshot.sh
@@ -1,7 +1,10 @@
+echo check deployment
+
 if [ "$TRAVIS_REPO_SLUG" == "TDesjardins/gwt-ol3" ] && \
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
 
+  echo deploy snapshot
   mvn -s ci/settings.xml clean deploy -Dmaven.test.skip=true -Dinvoker.skip=true
 fi
