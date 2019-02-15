79bdee828461aae28c25439357d558c9aac5100e
Tino Desjardins <tino.desjardins@arcor.de>
2019-02-15 08:29:17 +0100

    Add ol.has properties

---
+++ gwt-ol3-client/src/main/java/ol/has/Has.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright 2014, 2019 gwt-ol
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package ol.has;
+
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ *
+ */
+@JsType(isNative = true)
+public class Has {
+
+    @JsOverlay
+    private static final String PACKAGE_HAS = "ol.has";
+
+    /**
+     * @return The ratio between physical pixels and device-independent pixels (dips) on the device (window.devicePixelRatio).
+     */
+    @JsProperty(name = "DEVICE_PIXEL_RATIO", namespace = PACKAGE_HAS)
+    public static native double getDevicePixelRatio();
+
+    /**
+     * @return Is HTML5 geolocation supported in the current browser?
+     */
+    @JsProperty(name = "GEOLOCATION", namespace = PACKAGE_HAS)
+    public static native boolean isGeolocationEnabled();
+
+    /**
+     * @return true if browser supports touch events.
+     */
+    @JsProperty(name = "TOUCH", namespace = PACKAGE_HAS)
+    public static native boolean isTouchEnabled();
+
+}
