f9d7f1cac50e5956b2527ede4ca4a63196689168
Tino Desjardins <tino.desjardins@arcor.de>
2020-06-07 20:25:53 +0200

    Remove whitespaces

--- gwt-ol3-client/src/test/java/ol/OLUtilTest.java
+++ gwt-ol3-client/src/test/java/ol/OLUtilTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014, 2017 gwt-ol3
+ * Copyright 2014, 2020 gwt-ol
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ public class OLUtilTest extends GwtOLBaseTestCase {
             Style[] styles2 = {new Style(), new Style(), new Style()};
             assertTrue(styles1.length == 2);
             assertTrue(styles2.length == 3);
-            
+
             Style[] combinedStyles = OLUtil.concatArrays(styles1, styles2);
 
             assertNotNull(combinedStyles);
