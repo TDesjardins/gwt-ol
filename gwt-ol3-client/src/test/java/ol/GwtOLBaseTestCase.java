813ae6e03c1da4520c939a235c8a53adae9f2480
Tino Desjardins <tino.desjardins@arcor.de>
2023-05-24 15:12:28 +0200

    Remove polyfill
     - Polyfill isn't needed anymore because transitive update of HtmlUnit

--- gwt-ol3-client/src/test/java/ol/GwtOLBaseTestCase.java
+++ gwt-ol3-client/src/test/java/ol/GwtOLBaseTestCase.java
@@ -29,7 +29,6 @@ public abstract class GwtOLBaseTestCase extends BaseTestCase {
     public GwtOLBaseTestCase() {
 
         super(Arrays.asList(
-                "//polyfill.io/v3/polyfill.js?features=Blob,URL,Symbol&flags=always&version=3.46.0",
                 "//cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@main/dist/en/v6.6.1/build/ol.js"),
                 "ol.GwtOLTest",
                 10000);
